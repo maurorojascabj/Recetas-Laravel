f24b91580b222b1950d60486f3609737042d14e6
Mauro Rojas <maurorojascabj@gmail.com>
2021-02-17 12:01:14 -0300

    Se agrego un middleware para que no se pueda acceder a ninguna ruta, sin estar logueado previamente.

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\DB;
 
 class RecetaController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     /**
      * Display a listing of the resource.
      *
